161254f839801bc0705c2d319d67cf1b54ed9122
bruno <bruno@bruno-Inspiron-1525.(none)>
2011-06-20 11:07:23 -0300

    Ver uma pagina de professor

--- app/controllers/professores_controller.rb
+++ app/controllers/professores_controller.rb
@@ -1,5 +1,32 @@
 class ProfessoresController < ApplicationController
 	def view
-		profs=[]
+		# Mock do BD
+		profs = {
+			'bruno' => {
+				'nome' => 'Bruno Lima',
+				'descricao' => 'Um cara namoral'
+				},
+			'pedro' => {
+				'nome' => 'Pedro Alberto',
+				'descricao' => 'Um cara do Pará'
+				},
+			'luiz' => {
+				'nome' => 'Luiz Fernando J. Bacelar',
+				'descricao' => 'Um cara Janela'
+				},
+			'guilherme' => {
+				'nome' => 'Guilherme "O Foda" Fernandes',
+				'descricao' => 'Um cara babaca'
+				}
+		}
+		
+		# Testa se professor existe no Banco de Dados
+		if (profs.include? params[:nome])
+			# Atribuir os dados do BD as variáveis da View
+			@conteudo = profs[params[:nome]]
+		else
+			# Lançar 404
+		end
+			
 	end
 end
