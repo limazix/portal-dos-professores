814d23e96a3ed67f032430070e6aa335d3fb434b
unknown <Lets_Gui@.(none)>
2011-07-16 14:30:24 -0300

    Tempo nos Conteudos

--- app/controllers/conteudos_controller.rb
+++ app/controllers/conteudos_controller.rb
@@ -3,8 +3,13 @@ class ConteudosController < ApplicationController
     end
 
     def create
+	
+		time = Time.new
+		params[:conteudo]["texto"] =  "(" + time.strftime("%d/%m/%Y %H:%M:%S") + ") " + params[:conteudo]["texto"];
+	
         @prof = Professor.find(params[:professor_id])
-        @conteudo = @prof.conteudos.create(params[:conteudo])
+		
+		@conteudo = @prof.conteudos.create(params[:conteudo])
         redirect_to (root_path + (@prof.nome))
     end
     
