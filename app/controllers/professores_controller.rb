ef9a1c568e197369ce245988de50a738eb227316
Bruno Lima <blimacardoso@gmail.com>
2011-07-27 12:50:16 -0300

    Lendo email com net/pop

--- app/controllers/professores_controller.rb
+++ app/controllers/professores_controller.rb
@@ -1,4 +1,7 @@
+require 'net/pop'
+
 class ProfessoresController < ApplicationController
+	
 	def view
         
         @prof = Professor.find_by_nome(params[:nome])
@@ -7,6 +10,22 @@ class ProfessoresController < ApplicationController
            @prof = Professor.create!(:nome => params[:nome], :nome_real => params[:nome])
         end
 
+		username = 'portaldosprofessoresufrj@gmail.com'
+		password = 'portaldosprofessores'
+
+		Net::POP3.enable_ssl(OpenSSL::SSL::VERIFY_NONE)
+		Net::POP3.start('pop.gmail.com', 995, username, password) do |pop|
+		if pop.mails.empty?
+		  puts 'No mail.'
+		else
+		  pop.each_mail do |mail|
+		  	#p mail.subject
+			@header = mail.header
+			#p mail.pop
+		  end
+		end
+		end
+		
         respond_to do |format|
             format.html
         end
